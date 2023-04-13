aacc5089d16296bdd5c30c44ac4b2832e28fa5c8
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-04-13 19:39:46 -0400

    feat: homeworks

--- homework/03-more-image-processing-with-hof/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { Image } from "../include/image.js";
-import { combineThree, imageBlur } from "./moreImageProcessing.js";
-
-imageBlur(Image.loadImageFromGallery()).show();
-combineThree(Image.loadImageFromGallery()).show();
